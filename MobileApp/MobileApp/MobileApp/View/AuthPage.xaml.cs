563ec3d5c487675d5c98b06e6fc870790dbe4d84
Igor <gigyga@gmail.com>
2020-03-24 14:12:14 +0300

    First commit

---
+++ MobileApp/MobileApp/MobileApp/View/AuthPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using MobileApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileApp.View
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class AuthPage : ContentPage
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ MobileApp/MobileApp/MobileApp/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MobileApp.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobileApp.ViewModel
+{
+    class MainPageViewModel
+    {
+        public Command ExitCommand;
+        public Command LoginCommand => new Command<INavigation>(async (n) => 
+        {
+            await n.PushModalAsync(new NavigationPage(new OrderPage()));
+        });
+    }
+}
